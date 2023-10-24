cf6cbe31f7d5aee7211a92a625e11785489f67fd
bhycode <badreddynelhirch2@gmail.com>
2023-10-24 16:02:07 +0100

    MORE WORK.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,19 @@ function openAnswer(num) {
         faq_answer.style.display = 'none';
     }
 }
+
+
+
+let burger_menu = document.getElementById("burger-menu");
+let burger_nav = document.getElementById("burger-nav");
+let burger_icon = document.getElementById("burger-icon");
+
+
+burger_icon.addEventListener("click", function() {
+
+    if(burger_nav.style.display === 'none') {
+        burger_nav.style.display = 'block';
+    } else {
+        burger_nav.style.display = 'none';
+    }
+});
